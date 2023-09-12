4ae8b8b6010d880c8e7e1da68cafa223ef9ade32
mkskh <maksym.kharchenko@dci-student.org>
2023-09-12 14:25:09 +0200

    first done

---
+++ task01.py
@@ -0,0 +1,8 @@
+from datetime import datetime, timedelta
+
+# Using the variable called current_datetime, subtract 15 days from the current time.
+
+current_datetime = datetime(2021, 7, 8)
+time_minus_15days = current_datetime - timedelta(days = 15)
+reformat = time_minus_15days.strftime('%x')
+print(reformat)
