22425d8b2cc59b47826aae871825e4ac44023891
mkskh <maksym.kharchenko@dci-student.org>
2023-09-13 14:23:17 +0200

    added changes

--- task02.py
+++ task02.py
@@ -3,6 +3,6 @@ from datetime import datetime, timedelta
 # Using the variable called current_datetime, add 7 days to your current day
 
 current_datetime = datetime(2021, 7, 8)
-time_minus_15days = current_datetime + timedelta(days = 7)
-reformat = time_minus_15days.strftime('%Y-%m-%d')
+time_plus_15days = current_datetime + timedelta(days = 7)
+reformat = time_plus_15days.strftime('%Y-%m-%d')
 print(reformat)
